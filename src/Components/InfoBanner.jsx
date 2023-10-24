21a9275f5433969e915234d015af27c98f576a43
Tusk17 <125697090+JCCVallejo@users.noreply.github.com>
2023-10-24 12:04:16 -0600

    Update InfoBanner.jsx

--- src/Components/InfoBanner.jsx
+++ src/Components/InfoBanner.jsx
@@ -8,10 +8,14 @@ const InfoBanner = () => {
                     <div className="col-md-8 text-center">
                         <h2 className="text-uppercase mb-3">¿Quién soy?</h2>
                         <p className="lead mb-4">
-                        Una persona que ha desempeñado roles como practicante y desarrollador Jr. Me enfoco y prefiero trabajar con Back end, sin embargo
-                        soy consciente de la importancia de un buen diseño front end y la experiencia del usuario por lo que también me desempeño un poco en esa area.
-                        Soy principiante en los procesos ETL, pero son un area de interes para mi dada la automatización que se les puede integrar, un ejemplo de esto
-                        es la extracción de datos mediante web scrapping, la limpieza y ordenamiento usando algoritmos, para terminar con la subida por lotes a una BD.
+                        Soy un profesional con experiencia en roles como practicante y desarrollador junior. 
+                        Mi preferencia y enfoque principal es el desarrollo de Back end, aunque reconozco la importancia
+                        de un diseño front end atractivo y una experiencia de usuario óptima, por lo que también tengo 
+                        habilidades en esa área. Aunque soy principiante en los procesos ETL, me atraen enormemente 
+                        debido a las posibilidades de automatización que ofrecen. Un ejemplo de esto es la extracción 
+                        de datos mediante web scrapping, seguido de la limpieza y ordenamiento de estos datos utilizando 
+                        algoritmos, para finalmente cargarlos en lotes a una base de datos. Esta es un área en la que 
+                        estoy interesado en profundizar y desarrollar aún más mis habilidades.
                         </p>
                         <a
                             href="https://mnf.red/jccv98"
